d42f32a69140359462802cc3d832b21e08ad2465
Kesyza <kesyza_210@smkassalaambandung.sch.id>
2021-11-12 12:52:18 +0000

    crud seeder laravel

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    // memberikan akses data apa saja yang bisa di lihat
+    protected $visible = ['name'];
+    
+    // memberikan akses data apa saja yang  bisa diisi
+    protected $fillable = ['name'];
+
+    // mencatat waktu pembuatan dan update data otomatis
+    public $timestamps = true;
+
+    // membuat relasi one to many
+    public function books()
+    {
+        // data model "Author" bisa memiliki banyak data
+        // dari model "Book"  melalui fk "author_id"
+        $this->hasMany('App\Models\Book', 'author_id');
+    }
 }
